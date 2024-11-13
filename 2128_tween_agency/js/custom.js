daba42ba4edb841b15eaf9890e4d5b636cfda8d7
NTSHIKALA <165667929+NTSHIKALA@users.noreply.github.com>
2024-11-13 09:53:42 +0100

    Add files via upload

---
+++ 2128_tween_agency/js/custom.js
@@ -0,0 +1,30 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $(".navbar").addClass("sticky-nav");
+        } else {
+            $(".navbar").removeClass("sticky-nav");
+        }
+    });
+
+    // BACKSTRETCH SLIDESHOW
+    $('#section_1').backstretch([
+      "images/slide/aa3.jpg", 
+      "images/slide/aa1.jpg",
+      "images/slide/aa2.jpeg"
+    ],  {duration: 2000, fade: 750});
+    
+  })(window.jQuery);
+
+
